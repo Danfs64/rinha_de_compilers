1f0d7ef8a9df6dc10b22bb7f076da135272a61a4
Danfs <trabalhosdosilva@gmail.com>
2023-09-22 20:59:44 -0300

    fixed

--- src/interpreter.ts
+++ src/interpreter.ts
@@ -1,15 +1,15 @@
 import type { BinaryOp, File, Function, Term } from "./schema.js";
 
-/** Coisas que podem ser retornadas por uma expressão válida de uma `.rinha`.
- * 
- * Notas:
- * - `Function`, o nó da AST, está aqui inalterado pois não decidi ainda como representar funções internamente
- *     - Cogitando colocar uma função anônima, seria bem ajeitadinho
- * -  Tuplas estão representadas como tuplas de `Term`, ao invês de `ReturnType`, na espectativa de avaliações nelas serem lazy
- */
-type ReturnType = string | number | boolean | [Term, Term] | Function
-
 type NameTable = Map<string, ReturnType>
+
+/** Coisas que podem ser retornadas por uma expressão válida de uma `.rinha`.
+ */
+type ReturnType = string | number | boolean | [ReturnType, ReturnType] | Callable
+
+type Callable = {
+    fn: Function
+    local_nametable: NameTable
+}
 
 /** Interpreta o conteúdo da AST de um `.rinha`
  * 
@@ -27,15 +27,15 @@ function evaluate(term: Readonly<Term>, nametable: Map<string, ReturnType>): {re
             // Valores primitivos são retornados normalmente
             return {ret: term.value, nametable}
         case "Tuple":
-            return {ret: [term.first, term.second], nametable}
+            return {ret: [evaluate(term.first, nametable).ret, evaluate(term.second, nametable).ret], nametable}
         case "First":
         case "Second":
             // O enunciado fala que First e Second sempre serão chamados em tuplas, então a gente pode fazer essa barbaridade de type assertion
             // Se não fosse o caso, teria que fazer um `if` que levantaria um erro em qqr termo não-tupla, dispensando o uso do `as`
-            const {ret: [fst, snd]} = evaluate(term.value, nametable) as {ret: [Term, Term], nametable: NameTable}
+            const {ret: [fst, snd]} = evaluate(term.value, nametable) as {ret: [ReturnType, ReturnType], nametable: NameTable}
             return term.kind == "First"
-                ? evaluate(fst, nametable)
-                : evaluate(snd, nametable)
+                ? {ret: fst, nametable}
+                : {ret: snd, nametable}
         case "Binary":
             return {ret: do_binary_op(term.lhs, term.rhs, term.op, nametable), nametable}
         case "If":
@@ -44,27 +44,28 @@ function evaluate(term: Readonly<Term>, nametable: Map<string, ReturnType>): {re
                 : evaluate(term.otherwise, nametable)
         case "Print":
             const term_to_print = evaluate(term.value, nametable)
-            console.log(to_print(term_to_print.ret, nametable))
+            console.log(to_print(term_to_print.ret))
             return term_to_print
         case "Let":
             const new_nametable = new Map(nametable)
-            new_nametable.set(term.name.text, evaluate(term.value, nametable).ret)
+            const value = evaluate(term.value, nametable).ret
+            nametable.set(term.name.text, value)
+            new_nametable.set(term.name.text, value)
             return evaluate(term.next, new_nametable)
         case "Function":
-            return {ret: term, nametable}
+            return {ret: {fn: term, local_nametable: nametable}, nametable}
         case "Call":
             return {ret: call_function(term.callee, term.arguments, nametable), nametable}
         case "Var":
             const ret = nametable.get(term.text)
-            if(ret !== undefined) {
-                return {ret, nametable}
-            } else {
+            if(ret === undefined) {
                 throw Error(`Variável ${term.text} não foi declarada`)
+            } else {
+                return {ret, nametable}
             }
     }
 }
 
-// TODO Talvez tenha que fazer checagens de operações inválidas? (str GTE number)
 function do_binary_op(lhs: Readonly<Term>, rhs: Readonly<Term>, op: BinaryOp, nametable: NameTable): string | number | boolean {
     switch(op) {
         case "Add": return (evaluate(lhs, nametable).ret as any) +  (evaluate(rhs, nametable).ret as any)
@@ -83,7 +84,7 @@ function do_binary_op(lhs: Readonly<Term>, rhs: Readonly<Term>, op: BinaryOp, na
     }
 }
 
-function to_print(value: ReturnType, nametable: NameTable): string {
+function to_print(value: ReturnType): string {
     switch(typeof value) {
         case "string":
             return value
@@ -94,29 +95,27 @@ function to_print(value: ReturnType, nametable: NameTable): string {
                 ? "true"
                 : "false"
         case "object":
-            return isFunction(value)
+            return isCallable(value)
                 ? "<#closure>"
-                : `(${evaluate(value[0], nametable)}, ${evaluate(value[1], nametable)})`
-        case "function":
-        default:
-            return value
+                : `(${to_print(value[0])}, ${to_print(value[1])})`
     }
 }
 
-function isFunction(x: ReturnType): x is Function {
-    return (x as Function)?.kind == "Function"
+function isCallable(x: ReturnType): x is Callable {
+    return (x as Callable)?.fn !== undefined
 }
 
 function call_function(callee: Readonly<Term>, args: readonly Readonly<Term>[], nametable: NameTable): ReturnType {
-    const {ret: fn} = evaluate(callee, nametable)
-    if(!isFunction(fn)) {
-        throw Error(`'${fn}' is not callable!`)
+    const {ret: callable} = evaluate(callee, nametable)
+    if(!isCallable(callable)) {
+        throw Error(`'${callable}' is not callable!`)
     }
+    const {fn, local_nametable} = callable
+    const new_nametable = new Map(local_nametable)
     if(args.length != fn.parameters.length) {
         throw Error(`Incorrect number of arguments. Expected ${fn.parameters.length}. Got ${args.length}.`)
     }
 
-    const new_nametable = new Map(nametable)
     for(const [idx, p] of fn.parameters.entries()) {
         new_nametable.set(p.text, evaluate(args[idx], nametable).ret)
     }
